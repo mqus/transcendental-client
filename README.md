ecaf9ccfd0a7322c1978df87b8910d9e8c1f730d
Markus Richter <marku.online@googlemail.com>
2017-04-29 18:46:00 +0200

    ready first release

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # transcendental-client
-A Client for the transcendental Clipboard Server
+A Client-Library and a simple implementation of a Client for the transcendental Clipboard Server
+
+The Client now mostly works but there may be quite some Bugs which i have overlooked.
+
+To let the client run, a running server is necessary, the server is provided in [https://github.com/mqus/transcendental](https://github.com/mqus/transcendental)
+
+Current Version: 0.6.0
+## TODO
+  - [ ] Tests
+  - [ ] One-Transmission-Text-Transfer (TEXT)
+  - [ ] Maybe respect and use SendRetryPolicy (current policy: retry, even if the connection is lost for a short time)
+  - [ ] An advanced client for the Tray
+  - [ ] A client for android
+  
+## Dependencies
+  - Java 1.7 (openjdk or Oracle Java)
+
+for building:
+  - Gradle >=3.4 (older may also work, couldn't test it)
+
+
+## Build instructions:
+To run the client you can either (1) run the client from source or (2) build a distribution package and run from that.
+### (1) Run the Client from Source
+<pre>$ gradle run</pre>
+This will use the default options, but you can supply commandline Options with the following way:
+<pre>$ gradle run -PappArgs="['server:port', '--room', 'MySecretRoom']"</pre>
+### (2) Compile to the distribution binary
+<pre>$ gradle distZip</pre>
+This will create a zip file in build/distributions, which you can unpack wherever you like. You then have to run bin/transcendental-client (or bin/transcendental-client.bat on Windows)
+<pre>
+/tmp/uyrcuyr/transcendental-client-0.6.0 $ bin/transcendental-client --help
+Transcendental-SimpleClient v0.6.0
+	a simple deamon-like Client for sharing the local Clipboard with other Devices.
+Usage: transcendental-client [OPTIONS] [<server>:<port>]
+  -h	--help	 	 outputs this help
+  -v	--version	 	 outputs the version
+  -s	--server	<server>[:<port>]	 specifies the server which distributes the data, default:localhost
+  -p	--port	<port>	 specifies the server port, default:19192
+  -r|-k	--password | --room | --key	<passwd>	 specifies the encryption keyword and at the same time the room*, default:RaumRaumRaumRaum
+
+   (*) a room is the separation unit in which the server distributes the clipboard content, there can be multiple rooms on a server.
+</pre>
--- src/main/java/transcendental/client/example/SimpleClient.java
+++ src/main/java/transcendental/client/example/SimpleClient.java
@@ -8,6 +8,8 @@ import java.security.InvalidKeyException;
  * Created by markus on 26.01.17.
  */
 public class SimpleClient implements StateChangeListener{
+	public static final String ver = "v0.6.0";
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -23,11 +25,11 @@ public class SimpleClient implements StateChangeListener{
 	Connection conn;
 	Client client;
 
-	public SimpleClient() throws InvalidKeyException {
+	public SimpleClient(String server, int port, String passwd) throws InvalidKeyException {
 		//setup
-		conn = new Connection("localhost",19192);
+		conn = new Connection(server,port);
 		conn.setStateChangeListener(this);
-		client = new Client("RaumRaumRaumRaum", conn);
+		client = new Client(passwd, conn);
 		client.setStateChangeListener(this);
 
 	}
@@ -54,8 +56,73 @@ public class SimpleClient implements StateChangeListener{
 
 
 	public static void main(String[] args) throws InvalidKeyException {
-		SimpleClient sc = new SimpleClient();
+		//defaults:
+		String server="localhost", passwd="RaumRaumRaumRaum";
+		int port=19192;
+
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].startsWith("-")) {
+				switch(args[i]) {
+					case "-p":
+					case "--port":
+						port = Integer.valueOf(args[++i]);
+						break;
+					case "-s":
+					case "--server":
+						server = args[++i];
+						if(server.contains(":")){
+							//take the argument as a server:port option:
+							String[] a=server.split(":",2);
+							server=a[0];
+							port=Integer.valueOf(a[1]);
+						}
+						break;
+					case "-r":
+					case "-k":
+					case "--password":
+					case "--key":
+					case "--room":
+						passwd = args[++i];
+						break;
+					case "-h":
+					case "--help":
+						showHelp();
+						return;
+					case "-v":
+					case "--version":
+						showVersion(false);
+						return;
+				}
+			}else{
+				//take the argument as a server:port option:
+				String[] a=args[i].split(":",2);
+				server=a[0];
+				port=Integer.valueOf(a[1]);
+			}
+
+		}
+
+		SimpleClient sc = new SimpleClient(server, port, passwd);
 		sc.go();
+	}
+
+
+	private static void showHelp(){
+		showVersion(true);
+		System.out.println("Usage: transcendental-client [OPTIONS] [<server>:<port>]");
+		System.out.println("  -h\t--help\t \t outputs this help");
+		System.out.println("  -v\t--version\t \t outputs the version");
+		System.out.println("  -s\t--server\t<server>[:<port>]\t specifies the server which distributes the data, default:localhost");
+		System.out.println("  -p\t--port\t<port>\t specifies the server port, default:19192");
+		System.out.println("  -r|-k\t--password | --room | --key\t<passwd>\t specifies the encryption keyword and at the same time the room*, default:RaumRaumRaumRaum");
+		System.out.println("\n   (*) a room is the separation unit in which the server distributes the clipboard content, there can be multiple rooms on a server.");
+	}
+
+	private static void showVersion(boolean extended){
+		System.out.println("Transcendental-SimpleClient "+ver);
+		if(extended){
+			System.out.println("\ta simple deamon-like Client for sharing the local Clipboard with other Devices.");
+		}
 	}
 
 }
