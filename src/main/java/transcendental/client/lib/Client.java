ab873e462b629dae7cefd9b94b913c22b1b2439e
Markus Richter <marku.online@googlemail.com>
2017-04-29 16:28:51 +0200

    removed temporary debugging messages

--- src/main/java/transcendental/client/lib/Client.java
+++ src/main/java/transcendental/client/lib/Client.java
@@ -210,14 +210,11 @@ public class Client implements FlavorListener, Transferable, ClipboardOwner {
 		if(this.conn.getState() == ConnState.CONNECTED)
 			changeState(ClientState.DATA_ON_THIS_CLIENT);
 			if(sendPolicy.shouldSend(clipboard.getContents(this))) {
-				System.out.println("sending...");
-
 				DataFlavor[] flavors = clipboard.getAvailableDataFlavors();
 				byte[] data = Util.serializeFlavors(flavors);
 
 				byte[] pkg = packager.packCopy(data);
 				conn.reliableSend(pkg, false);
-				System.out.println("sent");
 
 			}
 	}
