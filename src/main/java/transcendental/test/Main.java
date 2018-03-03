fd0bc633e21c52a5e47c13105fa45e51e8e8b7a3
Markus Richter <marku.online@googlemail.com>
2018-03-03 21:09:23 +0100

    changed debug-main to return the current clipboard status

--- src/main/java/transcendental/test/Main.java
+++ src/main/java/transcendental/test/Main.java
@@ -1,11 +1,13 @@
 package transcendental.test;
 
+import transcendental.client.lib.Util;
+
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.FlavorEvent;
-import java.awt.datatransfer.FlavorListener;
+import java.awt.datatransfer.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
+import java.util.Arrays;
 
 /**
  * Created by markus on 26.01.17.
@@ -17,6 +19,40 @@ public class Main {
 		System.out.println("_____");
 		System.out.println(System.getProperty("os.name"));
 		System.out.println("_____");
+		final Clipboard x = Toolkit.getDefaultToolkit().getSystemClipboard();
+		x.addFlavorListener(new FlavorListener() {
+			@Override
+			public void flavorsChanged(FlavorEvent e) {
+				System.err.println("----------------------");
+				for(DataFlavor dataFlavor : x.getAvailableDataFlavors()) {
+					if(dataFlavor.isRepresentationClassInputStream()) {
+						System.out.println(dataFlavor.getMimeType());
+						if(dataFlavor.isMimeTypeEqual("text/uri-list; class=java.io.InputStream")) {
+							try {
+								InputStream data = (InputStream) x.getData(dataFlavor);
+								byte[] buffer = new byte[1024*1024];
+								int read = data.read(buffer);
+								if(read==1024*1024)
+									System.err.println("BIG");
+								data.close();
+								System.out.println("\t"+new String(Arrays.copyOf(buffer,read)));
+							} catch(UnsupportedFlavorException e1) {
+								e1.printStackTrace();
+							} catch(IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		});
+		while(true) {
+			try {
+				Thread.sleep(10000L);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		//byte[] p = Package.createHelloPackage("Raum1".getBytes());
 		//System.out.println(p);
 		//System.out.println(new String(p,"UTF-8"));
